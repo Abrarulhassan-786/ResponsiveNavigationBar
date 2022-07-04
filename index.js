e2e30a261a08f676047b7e113a0b5bbbcdcfcfb5
Abrar Hassan <107148226+Abrar-Hassan-Programming@users.noreply.github.com>
2022-07-04 22:18:23 +0500

    Responsive Nav Bar
    I have made Responsive Navbar, Hopefully, It will be helpful for you. I have made using Html,CSS,JS.

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const mobile_nav = document.querySelector(".mobile-nav-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
